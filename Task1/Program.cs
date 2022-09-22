d17e0076ef5026306d064d362cae9036407f9f96
Olgavav <petrova-olga@myrambler.ru>
2022-09-22 17:45:53 +0400

    добавили первое задание

---
+++ Task1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число два числа: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2)
+{Console.WriteLine("number max="+number1);
+Console.WriteLine("number min="+number2);}
+else
+{Console.WriteLine("number min="+number1);
+Console.WriteLine("number max="+number2);}
