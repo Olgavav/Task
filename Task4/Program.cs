57490abd494a5ba43038b21328e305e40173daba
Olgavav <petrova-olga@myrambler.ru>
2022-09-22 19:20:25 +0400

    добавили четвертое  задание

---
+++ Task4/Program.cs
@@ -0,0 +1,9 @@
+﻿//Напишите программу,
+//которая на вход принимает число (n), а на выход показывает все четные числа от 2 до N
+Console.WriteLine("Введите число: ");
+Console.Write("n=");
+int n= Convert.ToInt32(Console.ReadLine());
+for(int i=1; i<n; i++)
+{if(i%2 == 0)
+
+Console.WriteLine(i);}
